9bbb64b851d06e022f24ca1df17e1b003ca9f43b
Marcos Andre Da Silva <marcus.ads37@gmail.com>
2018-09-15 21:41:47 -0300

    Chamar uma Funcao Simple No Java Script

---
+++ singleFuncion/helloFuncition.js
@@ -0,0 +1,10 @@
+/**
+* Java Script - Segunda Aula: funçao Simples
+* @author Marcos Andre Da Silva
+*/
+
+function BemVindo(){
+	var nome;
+	nome = prompt("Qual e o seu Nome");
+	alert(" Bem Vindo " + nome );
+}	
